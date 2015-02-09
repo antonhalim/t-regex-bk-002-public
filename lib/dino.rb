a7bc591c71c16ae277a7953b281cdd0e2a3cb69f
axton21 <axton_n@yahoo.com>
2015-02-09 15:27:46 -0500

    a dive into the jpark

--- lib/dino.rb
+++ lib/dino.rb
@@ -1,15 +1,30 @@
 def is_dinosaur?(animal)
-  # code here
+    animal.include?("saur")
+
 end
 
 def will_sara_eat_it?(plant)
-  # code here
+     if (plant =~ /^[c]/i) == 0
+      return true
+    else
+      return false
+    end
 end
 
 def match_raptor(animal)
-  # code here
+    if (animal =~ /.+raptor$/) == nil
+      "#{animal} isn't a raptor"
+    else
+      animal
+    end
+
 end
 
 def match_feather_like(phrase)
-  # code here
+    if (phrase =~ /plume|fluff|quill|feathers/) == nil
+      'no match found'
+    else
+      phrase
+    end
+
 end
